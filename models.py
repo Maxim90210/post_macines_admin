0bbb8e7e947ed8df07e04dcc7b5cc4d3cc17fe77
Maxim90210 <150370357+Maxim90210@users.noreply.github.com>
2024-08-01 21:35:00 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -1,18 +1,4 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Locker(models.Model):
-    locker_id = models.AutoField(primary_key=True)
-    location = models.CharField(max_length=100)
-    status = models.CharField(max_length=20)  # Наприклад, 'active', 'inactive'
-
-    def __str__(self):
-        return f"{self.location} - {self.status}"
-
-class Compartment(models.Model):
-    compartment_id = models.AutoField(primary_key=True)
-    locker = models.ForeignKey(Locker, on_delete=models.CASCADE, related_name='compartments')
-    size = models.CharField(max_length=20)  # Наприклад, 'small', 'medium', 'large'
-    status = models.CharField(max_length=20)  # Наприклад, 'available', 'occupied'
-
-    def __str__(self):
-        return f"Locker {self.locker.locker_id} - Compartment {self.compartment_id} ({self.size})"
+class CustomUser(AbstractUser):
+    pass
--- urls.py
+++ urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
-from views import locker_detail
+from .views import register, CustomLoginView
 
 urlpatterns = [
-    path('locker/<int:locker_id>/', locker_detail, name='locker_detail'),
-]
-
-
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('lockers/', include('lockers.urls')),
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
 ]
